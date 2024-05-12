a7cc55ea405ffcc3e410e87def7462d04bd3f461
dheepak-GH-action <mail2gdek@email.com>
2024-05-12 05:30:08 +0000

    deploy: dheepakg/static-site@1def67adc5eb7e73285031efef4bb3369cef0f76

--- visuals/nifty50/nifty50.js
+++ visuals/nifty50/nifty50.js
@@ -1,6 +1,6 @@
 // Set dimensions & margins
 
-const margin = { top: 70, right: 30, bottom: 40, left: 80};
+const margin = { top: 70, right: 30, bottom: 40, left: 75};
 const width = 1200 - margin.left - margin.right;
 const height = 500 - margin.top - margin.bottom ;
 
